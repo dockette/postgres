2d05e88b4bec77a02f465294b95f906de4a3773b
Milan Felix Šulc <rkfelix@gmail.com>
2020-01-06 18:19:43 +0100

    WIP

--- 11/init-scripts/init-czech-dict.sql
+++
@@ -1,6 +0,0 @@
-CREATE TEXT SEARCH DICTIONARY cspell
-   (template=ispell, dictfile = czech_unaccent, afffile=czech_unaccent, stopwords=czech_unaccent);
-CREATE TEXT SEARCH CONFIGURATION cs (copy=english);
-ALTER TEXT SEARCH CONFIGURATION cs
-ALTER MAPPING FOR word, asciiword WITH cspell, simple;
-CREATE EXTENSION unaccent;
